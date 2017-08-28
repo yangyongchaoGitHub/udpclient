7529c2d588f2c3bc9b55a8bae8a038294f2fce39
yyy <1123096216@qq.com>
2017-08-28 20:50:15 +0800

    解决了占用cpu过高的问题，删除了无用代码。

--- UdpClient/src/com/wenyun/Main.java
+++ UdpClient/src/com/wenyun/Main.java
@@ -14,39 +14,20 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
-	private static DatagramPacket sendPacket;
-	private static DatagramSocket socket;
 	private static String ip = "114.115.143.70";
 	private static int port = 10561;
-	private static DatagramChannel receiveChannel;
 	private static DatagramChannel sendChannel;
 	private static Selector selector;
 
 	public static void main(String[] args) {
-		//ByteBuffer buffer = ByteBuffer.allocate(1024);
 		try {
-			// receiveChannel = DatagramChannel.open();
-			// receiveChannel.configureBlocking(false);
-			// socket = receiveChannel.socket();
-			// socket.bind(new InetSocketAddress(port));
-
-			// SelectionKey reveive = receiveChannel.register(selector,
-			// SelectionKey.OP_READ);
-
-			// 之后可采取和TCP/IP NIO中对selector遍历方式进行流信息的读取
-
 			sendChannel = DatagramChannel.open();
 			sendChannel.configureBlocking(false);
 			selector = Selector.open();
 			SocketAddress target = new InetSocketAddress(ip, port);
 			sendChannel.connect(target);
-			sendChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
-
-			// 阻塞写入流，若发送缓冲区已满，返回0，此时可继续注册OP_WRITE事件
-			// sendChannel.write(buffer);
-
+			sendChannel.register(selector, SelectionKey.OP_READ);
 		} catch (Exception e) {
-			socket = null;
 			e.printStackTrace();
 		}
 
@@ -55,7 +36,7 @@ public class Main {
 			System.exit(0);
 		}*/
 
-		LoopThread loopThread = new LoopThread(sendChannel, receiveChannel, selector);
+		LoopThread loopThread = new LoopThread(sendChannel, selector);
 		loopThread.start();
 
 		Scanner scanner = new Scanner(System.in);
@@ -65,27 +46,14 @@ public class Main {
 			if ("1".equals(str)) {
 				byte[] test = new byte[1];
 				test[0] = 0x01;
-				/*
-				 * DatagramPacket sendPacket = null; try { sendPacket = new
-				 * DatagramPacket(test, test.length, InetAddress.getByName(ip),
-				 * port); } catch (UnknownHostException e) { // TODO
-				 * Auto-generated catch block e.printStackTrace(); }
-				 */
+				
 				ByteBuffer b = ByteBuffer.allocate(1);
 				b.put(test);
-				// System.out.println("loopThread send 1");
 				loopThread.send(b);
-				// System.out.println("loopThread send 2");
 
 			} else if ("2".equals(str)) {
 				byte[] test = new byte[1];
 				test[0] = 0x02;
-				/*
-				 * DatagramPacket sendPacket = null; try { sendPacket = new
-				 * DatagramPacket(test, test.length, InetAddress.getByName(ip),
-				 * port); } catch (UnknownHostException e) { // TODO
-				 * Auto-generated catch block e.printStackTrace(); }
-				 */
 				ByteBuffer b = ByteBuffer.allocate(1);
 				b.put(test);
 
@@ -110,45 +78,27 @@ public class Main {
 
 class LoopThread extends Thread {
 	DatagramChannel send;
-	DatagramChannel listen;
 	Selector selector;
 
-	public LoopThread(DatagramChannel send, DatagramChannel listen, Selector selector) {
+	public LoopThread(DatagramChannel send, Selector selector) {
 		this.send = send;
-		this.listen = listen;
 		this.selector = selector;
 	}
 
 	@Override
 	public void run() {
-		SelectionKey sKey = null;
 		while (true) {
 			int nKeys = 0;
 			try {
+				Thread.sleep(50);
 				nKeys = selector.select();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 
-			// System.out.println("test");
-
 			if (nKeys > 0) {
-				System.out.println("nKeys > 0");
 				Set<SelectionKey> keys = selector.selectedKeys();
 				for (SelectionKey key : keys) {
-					System.out.println("test " + key);
-					// 对于发生连接事件
-					/*
-					 * if(key.isConnectable()) { SocketChannel sc =
-					 * (SocketChannel)key.channel();
-					 * sc.configureBlocking(false);
-					 * //注册感兴趣的IO事件，通常不直接注册写事件，在发送缓冲区未满的情况下
-					 * //一直是可写的，所以如果注册了写事件，而又不写数据，则很容易造成CPU消耗100% sKey =
-					 * sc.register(selector, SelectionKey.OP_READ);
-					 * 
-					 * //完成连接的建立 sc.finishConnect(); } else
-					 */
 					System.out.println(key.isConnectable() + " " + key.isWritable() + " " + key.isReadable() + " "
 							+ key.isAcceptable());
 					if (key.isConnectable()) {
@@ -156,29 +106,24 @@ class LoopThread extends Thread {
 							System.out.println("key.isConnectable()");
 							send.register(selector, SelectionKey.OP_WRITE | SelectionKey.OP_READ);
 						} catch (ClosedChannelException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
 					}
 					if (key.isReadable()) {
 						// 有流可读取
-
 						System.out.println("test 2");
-						ByteBuffer buffer = ByteBuffer.allocate(1024);
-						//SelectableChannel sc 
+						ByteBuffer buffer = ByteBuffer.allocate(10);
 						DatagramChannel sc = (DatagramChannel) key.channel();
-						//SocketChannel sc = (SocketChannel) key.channel();
 						int readBytes = 0;
 
 						try {
 							int ret = 0;
 							try {
-								// 读取目前可读的流，sc.read(buffer)返回的是成功复制到bytebuffer中
-								// 的字节数，为阻塞操作，值可能为0，若到流结尾，返回-1
 								while ((ret = sc.read(buffer)) > 0) {
 									readBytes += ret;
 								}
-								System.out.println(buffer.array().length);
+								System.out.println(buffer.array().length + new String(buffer.array()));
+								send.register(selector, SelectionKey.OP_WRITE | SelectionKey.OP_READ);
 							} catch (Exception e) {
 
 							} finally {
@@ -190,63 +135,17 @@ class LoopThread extends Thread {
 							}
 						}
 					}
-					/*
-					 * else if(key.isWritable())//可写入流 { //取消对OP_WRITE事件的注册
-					 * ByteBuffer buffer = ByteBuffer.allocate(1024);
-					 * key.interestOps(key.interestOps() &
-					 * (~SelectionKey.OP_WRITE)); SocketChannel sc =
-					 * (SocketChannel)key.channel();
-					 * 
-					 * //此步为阻塞操作，知道写入操作系统发送缓冲区或者网络IO出现异常
-					 * //返回的为成功写入的字节数，若缓冲区已满，返回0 int writeenedSize =
-					 * sc.write(buffer);
-					 * 
-					 * //若未写入，继续注册感兴趣的OP_WRITE事件 if(writeenedSize == 0) {
-					 * key.interestOps(key.interestOps() |
-					 * SelectionKey.OP_WRITE); } }
-					 */
 				}
-
 			}
-
-			/*
-			 * byte[] listen = new byte[100]; DatagramPacket listenPacket = new
-			 * DatagramPacket(listen, listen.length);
-			 * 
-			 * try { socket.receive(listenPacket);
-			 * 
-			 * } catch (IOException e) { // TODO Auto-generated catch block
-			 * e.printStackTrace(); }
-			 */
 		}
 	}
 
 	public void send(ByteBuffer buffer) {
-		// System.out.println("test send 1");
-		// int nKeys = 0;
 		buffer.flip();
 		try {
 			int writeenedSize = send.write(buffer);
-			/*
-			 * nKeys = selector.select(); if (nKeys > 0) { Set<SelectionKey>
-			 * keys = selector.selectedKeys(); System.out.println("test send 12"
-			 * ); for (SelectionKey key : keys) { if (key.isWritable()) {
-			 * 
-			 * } } }
-			 */
-
-			/*
-			 * if (packet != null) {
-			 * 
-			 * System.out.println("send"); ByteBuffer buffer =
-			 * ByteBuffer.allocate(2); int writeenedSize = send.write(buffer); }
-			 */
-			// listen.register(selector, SelectionKey.OP_WRITE);
-
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
 }
